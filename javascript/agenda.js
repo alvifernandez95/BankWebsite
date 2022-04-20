380d46c3e486b2b1797bf9a132ff85348f77e25f
Alvaro Fernandez <afernandez@itba.edu.ar>
2022-04-20 10:50:42 -0400

    Agregué la librería sweet alert al proyecto para poder poner una alerta cuando se agrega un nuevo contacto a la agenda.

--- javascript/agenda.js
+++ javascript/agenda.js
@@ -1,27 +1,33 @@
-const appendAgenda = (ev)=> {
-    ev.preventDefault(); 
-    let contacto = {
-        nombre: document.getElementById('nombreContacto').value, 
-        alias: document.getElementById('aliasContacto').value,
-        CBU: document.getElementById('CBUContacto').value
-    }
-    document.forms[0].reset();
-    let indexA = localStorage.length;
-    let indexB = localStorage.length + 1;
-    let indexC = localStorage.length + 2;
-    localStorage.setItem(indexA.toString(),contacto.nombre.toString());
-    localStorage.setItem(indexB.toString(),contacto.alias.toString());
-    localStorage.setItem(indexC.toString(),contacto.CBU.toString());
-    let tablaClientes = document.getElementById('tablaClientes');
-    let row = document.createElement('tr');
-    let html = '<td>' + contacto.nombre +'</td><td>' + contacto.alias + '</td><td>' + contacto.CBU+'</td>';
-    row.innerHTML = html;
-    tablaClientes.appendChild(row);
-}
+const appendAgenda = (ev) => {
+  ev.preventDefault();
+  let contacto = {
+    nombre: document.getElementById("nombreContacto").value,
+    alias: document.getElementById("aliasContacto").value,
+    CBU: document.getElementById("CBUContacto").value,
+  };
+  document.forms[0].reset();
+  let indexA = localStorage.length;
+  let indexB = localStorage.length + 1;
+  let indexC = localStorage.length + 2;
+  localStorage.setItem(indexA.toString(), contacto.nombre.toString());
+  localStorage.setItem(indexB.toString(), contacto.alias.toString());
+  localStorage.setItem(indexC.toString(), contacto.CBU.toString());
+  let tablaClientes = document.getElementById("tablaClientes");
+  let row = document.createElement("tr");
+  let html =
+    "<td>" +
+    contacto.nombre +
+    "</td><td>" +
+    contacto.alias +
+    "</td><td>" +
+    contacto.CBU +
+    "</td>";
+  row.innerHTML = html;
+  tablaClientes.appendChild(row);
+};
 
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('addContactButton').addEventListener('click', appendAgenda);
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("addContactButton")
+    .addEventListener("click", appendAgenda);
 });
-
-
--- javascript/inversiones.js
+++ javascript/inversiones.js
@@ -1,32 +1,34 @@
-const simularInversion = (ev)=> {
-    ev.preventDefault();
-    const r = 0.38;
-    const t = 1;
-    const n = 2;
-    let montoInicial = document.getElementById('montoInversion').value;
-    let montoFinal = montoInicial*(Math.pow((1 + (r/n)),(n*t)));
-    //console.log(montoFinal)
-    let mensaje1 = 'El monto acumulado tras 1 año es de $' + montoFinal +'.'
-    document.getElementById("resultadoSimulacion1").innerHTML = mensaje1;
+const simularInversion = (ev) => {
+  ev.preventDefault();
+  const r = 0.38;
+  const t = 1;
+  const n = 2;
+  let montoInicial = document.getElementById("montoInversion").value;
+  let montoFinal = montoInicial * Math.pow(1 + r / n, n * t);
+  //console.log(montoFinal)
+  let mensaje1 = "El monto acumulado tras 1 año es de $" + montoFinal + ".";
+  document.getElementById("resultadoSimulacion1").innerHTML = mensaje1;
+};
 
-}
+const simularInversion2 = (ev) => {
+  ev.preventDefault();
+  const r2 = 0.45;
+  const t2 = 1;
+  const n2 = 1;
+  let montoInicial2 = document.getElementById("montoInversion2").value;
+  let montoFinal2 = montoInicial2 * Math.pow(1 + r2 / n2, n2 * t2);
+  console.log(montoFinal2);
+  let mensaje2 = "El monto acumulado tras 1 año es de $" + montoFinal2 + ".";
+  document.getElementById("resultadoSimulacion2").innerHTML = mensaje2;
+};
 
-const simularInversion2 = (ev)=> {
-    ev.preventDefault();
-    const r2 = 0.45;
-    const t2 = 1;
-    const n2 = 1;
-    let montoInicial2 = document.getElementById('montoInversion2').value;
-    let montoFinal2 = montoInicial2*(Math.pow((1 + (r2/n2)),(n2*t2)));
-    console.log(montoFinal2)
-    let mensaje2 = 'El monto acumulado tras 1 año es de $' + montoFinal2 +'.'
-    document.getElementById("resultadoSimulacion2").innerHTML = mensaje2;
-}
-
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('simularInversionButton').addEventListener('click', simularInversion);
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("simularInversionButton")
+    .addEventListener("click", simularInversion);
 });
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('simularInversion2Button').addEventListener('click', simularInversion2);
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("simularInversion2Button")
+    .addEventListener("click", simularInversion2);
 });
