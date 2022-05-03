5de0e22270669e7f16c1759afc4ff6b9c93294ce
Alvaro Fernandez <afernandez@itba.edu.ar>
2022-05-03 08:57:46 -0400

    Agregando fetch al proyecto.

--- javascript/agenda.js
+++ javascript/agenda.js
@@ -1,33 +1,63 @@
-const appendAgenda = (ev) => {
-  ev.preventDefault();
-  let contacto = {
-    nombre: document.getElementById("nombreContacto").value,
-    alias: document.getElementById("aliasContacto").value,
-    CBU: document.getElementById("CBUContacto").value,
-  };
-  document.forms[0].reset();
-  let indexA = localStorage.length;
-  let indexB = localStorage.length + 1;
-  let indexC = localStorage.length + 2;
-  localStorage.setItem(indexA.toString(), contacto.nombre.toString());
-  localStorage.setItem(indexB.toString(), contacto.alias.toString());
-  localStorage.setItem(indexC.toString(), contacto.CBU.toString());
-  let tablaClientes = document.getElementById("tablaClientes");
+let nombre = document.getElementById("nombreContacto");
+let alias = document.getElementById("aliasContacto");
+let CBU = document.getElementById("CBUContacto");
+let btnAgregar = document.getElementById("addContactButton");
+let listaContactos = document.getElementById("tablaClientes");
+
+mostrarLista();
+
+function mostrarLista() {
+  for (let i = 0; i < localStorage.length; i += 3) {
+    let keyA = localStorage.key(i);
+    let keyB = localStorage.key(i + 1);
+    let keyC = localStorage.key(i + 2);
+    let itemA = localStorage.getItem(keyA);
+    let itemB = localStorage.getItem(keyB);
+    let itemC = localStorage.getItem(keyC);
+    addContacto(itemA, itemB, itemC);
+  }
+}
+
+function agregar_nuevo_contacto(itemA, itemB, itemC) {
+  if (itemA && itemB && itemC) {
+    addContacto(itemA, itemB, itemC);
+    let indexA = localStorage.length;
+    let indexB = localStorage.length + 1;
+    let indexC = localStorage.length + 2;
+    localStorage.setItem(indexA.toString(), itemA.toString());
+    localStorage.setItem(indexB.toString(), itemB.toString());
+    localStorage.setItem(indexC.toString(), itemC.toString());
+    Swal.fire({
+      title:"¡Genial!",
+      text:"¡Agregaste un nuevo contacto a tu agenda!",
+      icon:"success",
+      confirmButtonText:"Ok"
+    })
+
+  } else {
+    Swal.fire({
+      title:"Error",
+      text:"El registro ingresado podría estar vacío.",
+      icon:"error",
+      confirmButtonText:"Ok"
+    })
+  }
+}
+
+function addContacto(itemA, itemB, itemC) {
   let row = document.createElement("tr");
   let html =
-    "<td>" +
-    contacto.nombre +
-    "</td><td>" +
-    contacto.alias +
-    "</td><td>" +
-    contacto.CBU +
-    "</td>";
+    "<td>" + itemA + "</td><td>" + itemB + "</td><td>" + itemC + "</td>";
   row.innerHTML = html;
   tablaClientes.appendChild(row);
-};
+}
 
-document.addEventListener("DOMContentLoaded", () => {
-  document
-    .getElementById("addContactButton")
-    .addEventListener("click", appendAgenda);
+btnAgregar.addEventListener("click", () => {
+  let dato1 = nombre.value;
+  let dato2 = alias.value;
+  let dato3 = CBU.value;
+  agregar_nuevo_contacto(dato1, dato2, dato3);
+  nombre.value = "";
+  alias.value = "";
+  CBU.value = "";
 });
